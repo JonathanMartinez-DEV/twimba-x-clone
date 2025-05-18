56ec497ae770a97a0e1e47008b8019eb5a98f4d1
Jonathan Martinez <jonmar.dev@gmail.com>
2025-05-17 17:43:19 -0700

    feat: add tweet input textarea and build getFeedHTML function

---
+++ script.js
@@ -0,0 +1,35 @@
+import { tweetsData } from "./data.js";
+
+const tweetBtn = document.getElementById("tweet-btn");
+const tweetInput = document.getElementById("tweet-input");
+
+tweetBtn.addEventListener("click", () => {
+  console.log(tweetInput.value);
+});
+
+function getFeedHTML() {
+  let feedHTML = "";
+
+  tweetsData.forEach((tweet) => {
+    feedHTML += `
+        <div class="tweet">
+          <div class="tweet-inner">
+            <img src="${tweet.profilePic}" class="profile-pic" />
+            <div>
+              <p class="handle">${tweet.handle}</p>
+              <p class="tweet-text">${tweet.tweetText}</p>
+              <div class="tweet-details">
+                <span class="tweet-detail"> ${tweet.replies.length} </span>
+                <span class="tweet-detail"> ${tweet.likes} </span>
+                <span class="tweet-detail"> ${tweet.retweets} </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+  });
+
+  return feedHTML;
+}
+
+getFeedHTML();
